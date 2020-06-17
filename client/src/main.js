ea241b163aa556030b21718b821b8d0e3ca437e7
raphael-souza <raphael3130@gmail.com>
2020-06-17 07:34:28 -0300

    Vuex

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,21 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import DomainList from "./components/DomainList.vue";
 import DomainView from "./components/DomainView.vue";
+import Vuex from "vuex";
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
+// store é responsavel pelo gerenciamento de estado 
+const store = new Vuex.Store({
+  state: {
+    items: {
+      prefix: [],
+      sufix: []
+    },
+    domains: []
+  },
+  
+});
 
 const router = new VueRouter({
   routes: [
@@ -28,5 +41,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
