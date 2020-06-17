4d6882141edf19d153df62316c4cd31f0e1ebb89
raphael-souza <raphael3130@gmail.com>
2020-06-17 09:09:28 -0300

    mapState, mapActions

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -56,10 +56,8 @@
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import { mapState, mapActions } from "vuex";
 import AppItemList from "./AppItemList";
-import axios from "axios/dist/axios";
 
 export default {
   name: "DomainList",
@@ -70,104 +68,40 @@ export default {
     return {}
   },
   methods: {
-    addItem(item) {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation ($item: ItemInput) {
-              newItem: saveItem(item: $item) {
-                id
-                type
-                description
-              }
-            }
-          `,
-          variables: {
-            item
-          }
-        }
-      }).then( response => {
-        const query = response.data;
-        const newItem = query.data.newItem;
-        this.items[item.type].push(newItem);
-        this.generateDomains();
-      });
-    },
+
+    // addItem(item) {
+    //   this.$store.dispatch("addItem", item);
+    // },
+    // deleteItem(item) {
+    //   this.$store.dispatch("deleteItem", item);
+    // },
+    // getItems(type) {
+    //   this.$store.dispatch("getItems", type);
+      
+    // },
+    // generateDomains() {
+    //   this.$store.dispatch("generateDomains"); 
+    // }
+    ...mapActions(["addItem", "deleteItem", "getItems", "generateDomains"]),
+
     generate() {
-      this.domains = [];
-      for (const prefix of this.items.prefix) {
-        for (const sufix of this.items.sufix) {
-          this.domains.push(prefix + " " + sufix);
-        }
-      }
-    },
-    deleteItem(item) {
-      axios ({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation ($id: Int) {
-              seleted: deleteItem(id: $id)
-            }
-          `,
-          variables: {
-            id: item.id
+        this.domains = [];
+        for (const prefix of this.items.prefix) {
+          for (const sufix of this.items.sufix) {
+            this.domains.push(prefix + " " + sufix);
           }
         }
-      }).then(() => {
-        this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
-        this.generateDomains();
-        
-      });
-    },
-    getItems(type) {
-      return axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            query ($type: String) {
-              items: items (type: $type) {
-                id
-                type
-                description
-              }
-            }
-        `,
-        variables: {
-          type
-        }
-        }
-      }).then(response => {
-        const query = response.data;
-        this.items[type] = query.data.items;
-        console.log(query.data);
-      });
-    },
-    generateDomains() {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation {
-              domains: generateDomains {
-                name
-                checkout
-                available
-              }
-            }
-          `
-        }
-      }).then((response) => {
-        const query = response.data;
-        this.domains = query.data.domains;
-      });
-    }
-   
+      },
+  },
+  computed: {
+    // items() {
+    //   return this.$store.state.items;
+    // },
+    // domains() {
+    //   return this.$store.state.domains;
+    // }
+
+    ...mapState(["items", "domains"])
   },
  
   created() {
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import axios from "axios/dist/axios";
+import Vuex from "vuex";
+
 import App from './App.vue';
 import DomainList from "./components/DomainList.vue";
 import DomainView from "./components/DomainView.vue";
-import Vuex from "vuex";
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
@@ -16,7 +18,122 @@ const store = new Vuex.Store({
     },
     domains: []
   },
-  
+  mutations: {
+    addItem(state, payload) {
+      const { item, newItem } = payload;
+      state.items[item.type].push(newItem);
+
+    },
+    deleteItem(state, payload) {
+      const { item } = payload;
+      state.items[item.type].splice(state.items[item.type].indexOf(item), 1);
+
+    },
+    setItems(state, payload) {
+      const { type, items } = payload;
+      state.items[type] = items;
+    },
+    setDomains(state, payload) {
+
+    }
+
+  },
+  actions: {
+    async addItem(context, payload) {
+      const item = payload;
+      axios({
+        url: "http://localhost:4000",
+        method: "post",
+        data: {
+          query: `
+            mutation ($item: ItemInput) {
+              newItem: saveItem(item: $item) {
+                id
+                type
+                description
+              }
+            }
+          `,
+          variables: {
+            item
+          }
+        }
+      }).then( response => {
+        const query = response.data;
+        const newItem = query.data.newItem;
+        context.commit("addItem", { item, newItem });
+        context.dispatch("generateDomains");
+      });
+    },
+    async deleteItem(context, payload) {
+      const item = payload
+      axios ({
+        url: "http://localhost:4000",
+        method: "post",
+        data: {
+          query: `
+            mutation ($id: Int) {
+              seleted: deleteItem(id: $id)
+            }
+          `,
+          variables: {
+            id: item.id
+          }
+        }
+      }).then(() => {
+        context.commit("deleteItem", { item })
+        context.dispatch("generateDomains");
+        
+      });
+    },
+    async getItems(context, payload) {
+      const type = payload;
+
+      return axios({
+        url: "http://localhost:4000",
+        method: "post",
+        data: {
+          query: `
+            query ($type: String) {
+              items: items (type: $type) {
+                id
+                type
+                description
+              }
+            }
+        `,
+        variables: {
+          type
+        }
+        }
+      }).then(response => {
+        const query = response.data;
+        context.commit("setItems", { type, items: query.data.items });
+        console.log(query.data);
+      });
+    },
+    async generateDomains(context) {
+      axios({
+        url: "http://localhost:4000",
+        method: "post",
+        data: {
+          query: `
+            mutation {
+              domains: generateDomains {
+                name
+                checkout
+                available
+              }
+            }
+          `
+        }
+      }).then((response) => {
+        const query = response.data;
+        context.commit("setDomains", { domains: query.data.domains});
+        // context.state.domains = query.data.domains;
+      });
+    }
+  }
 });
 
 const router = new VueRouter({
